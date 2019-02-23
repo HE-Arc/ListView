15502232fd4184ffc7a5d3d796f0aaff0c467892
Steven Jeanneret <steven.jeanneret@outlook.com>
2019-02-23 11:23:29 +0100

    list only users team

--- ListView_backend/core/urls.py
+++ ListView_backend/core/urls.py
@@ -3,7 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from core import views
 
 urlpatterns = [
-    path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('teams/', views.TeamList.as_view()),
     path('teams/<int:pk>/', views.TeamDetail.as_view()),
--- ListView_backend/core/views.py
+++ ListView_backend/core/views.py
@@ -3,27 +3,28 @@ from rest_framework.response import Response
 from rest_framework import permissions
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from core.models import Team
+from core.models import Team, CustomUser
 from core.serializers import UserSerializer, TeamSerializer
 
 
 class TeamList(generics.ListCreateAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def list(self, request, **kwargs):
+        queryset = request.user.team
+        serializer = TeamSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-
-class UserList(generics.ListAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
