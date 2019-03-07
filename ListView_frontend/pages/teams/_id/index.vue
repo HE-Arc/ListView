782d6b8b73fbbc1bed638c6c32dd5aad3ccfb6bf
Steven Jeanneret <steven.jeanneret@outlook.com>
2019-03-07 16:20:09 +0100

    remove maj

---
+++ ListView_frontend/pages/teams/_id/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{team}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index.vue',
+    data () {
+      return {
+        team: ''
+      }
+    },
+    methods: {
+      downloadTeam () {
+        this.$axios.get('/api/teams/' + this.$route.params.id).then(response => {
+          console.log(response)
+          this.team = response.data
+        })
+      }
+    },
+    mounted () {
+      this.downloadTeam()
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
