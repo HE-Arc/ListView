c57f84184ad74cb6796860cb18092bcf2acaa9a0
Arzul Paul <paul.arzul@he-arc.ch>
2019-03-24 18:48:40 +0100

    test unlogged, #16

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+
+# cannot access teams if not logged in
+class TestUnlogged(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        response = self.client.get('/api/teams/')
+
+        self.assertEqual(response.status_code, 401)# unauthorized error
