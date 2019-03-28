be206f7b1a4f42a3298f1600a285a7fca175c264
Steven Jeanneret <steven.jeanneret@he-arc.ch>
2019-03-28 16:49:50 +0100

    Add createboard near other board #5

--- ListView_frontend/components/teamBoard.vue
+++ ListView_frontend/components/teamBoard.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="container border rounded mt-5">
+  <div class="container border rounded mt-5 text-center">
     <h1>{{name}}</h1>
     <div class="row">
-      <div class="col-md-3 text-center p-2" v-for="b in boards"
+      <div class="col-md-3 p-2" v-for="b in boards"
            @click="$router.push({name: 'board-boardId', params: {boardId: b.id}})">
         <div class="boardPane border border-secondary rounded py-5 bg-light">
           {{b.name}}
+        </div>
+      </div>
+      <div class="col-md-3 p-2">
+        <div class="boardPane border border-secondary rounded py-5 bg-light createBoard" @click="createBoard">
+          <i class="fas fa-plus"></i>
         </div>
       </div>
     </div>
@@ -18,6 +23,23 @@
     props: {
       name: String,
       boards: Array,
+      teamId: Number,
+    },
+    methods: {
+      createBoard () {
+        this.$swal({
+          title: 'New board',
+          inputPlaceholder: 'Board name',
+          input: 'text',
+          showCancelButton: true,
+        }).then(boardName => {
+          if(boardName.value) {
+            this.$axios.post('/api/boards/', {name: boardName.value, team_id: this.teamId}).then(result => {
+              this.$parent.getAllTeams()
+            })
+          }
+        })
+      }
     }
   }
 </script>
@@ -30,5 +52,13 @@
   .boardPane:hover {
     background-color: #7f828b !important;
     border-color: blue !important;
+  }
+
+  .createBoard {
+    opacity: 0.4;
+  }
+
+  .createBoard:hover {
+    opacity: 1;
   }
 </style>
