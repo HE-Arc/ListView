2bc8029492d073b6f7ee4fb64ad352f9ac056d80
Steven Jeanneret <steven.jeanneret@he-arc.ch>
2019-03-29 17:27:43 +0100

    create better home page

--- ListView_frontend/pages/index.vue
+++ ListView_frontend/pages/index.vue
@@ -1,8 +1,37 @@
 <template>
   <div>
-    <div>
-      <h1>Welcome on ListView</h1>
-      <p>ListView help you to manage your tasks with your friend or your colleagues</p>
+    <div class="container">
+      <h1 class="display-3 text-center mb-4">Welcome on ListView</h1>
+      <div class="row">
+        <div class="col-md-3"></div>
+        <div class="col-md-6 col-12 my-md-4">
+          <div class="card text-white bg-light mb-3">
+            <div class="card-header bg-dark text-center">Manage your team</div>
+            <div class="card-body text-dark">
+              <img src="/team.png" class="img-fluid" alt="">
+              <p class="card-text">Create teams and add people you want to collaborate with</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-12">
+          <div class="card text-white bg-light mb-3">
+            <div class="card-header bg-dark text-center">Manage your board</div>
+            <div class="card-body text-dark">
+              <img src="/boardteam.png" class="img-fluid" alt="">
+              <p class="card-text">Create boards in your teams to manage your projects</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-12">
+          <div class="card text-white bg-light mb-3">
+            <div class="card-header bg-dark text-center">Manage your task</div>
+            <div class="card-body text-dark">
+              <img src="/board.png" class="img-fluid" alt="">
+              <p class="card-text">Create to-do lists in your boards</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
